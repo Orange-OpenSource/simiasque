d34f9499e2ec24fbe3d916a1f484136566335b41
Arnaud Ruffin <arnaud.ruffin@orange.com>
2015-05-19 10:46:18 +0200

    Ready for publication

--- app/src/main/java/org/thisisafactory/simiasque/OverlayView.java
+++ app/src/main/java/org/thisisafactory/simiasque/OverlayView.java
@@ -1,3 +1,13 @@
+/*
+ * Simiasque
+ * Copyright (C) 2015 Orange
+ * Authors: arnaud.ruffin@orange.com
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package org.thisisafactory.simiasque;
 
 import android.content.Context;
